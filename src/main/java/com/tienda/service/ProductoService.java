e043984d0c3dc7838dddb99768d0e12762451ae5
marayav <marianavalenciano16@gmail.com>
2024-03-29 19:15:25 -0600

    Semana 11

--- src/main/java/com/tienda/service/ProductoService.java
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -4,10 +4,10 @@ import com.tienda.domain.Producto;
 import java.util.List;
 
 public interface ProductoService {
-    
+
     // Se obtiene un listado de productos en un List
     public List<Producto> getProductos(boolean activos);
-    
+
     // Se obtiene un Producto, a partir del id de un producto     
     public Producto getProducto(Producto producto);
 
@@ -17,7 +17,11 @@ public interface ProductoService {
 
     // Se elimina el producto que tiene el id pasado por parámetro   
     public void delete(Producto producto);
-    
-        public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
 
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+
+    //Lista de productos utilizando consultas con JPQL    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup);
+    
+    public List<Producto> metodoNativo(double precioInf, double precioSup);
 }
--- src/main/java/com/tienda/service/impl/ProductoServiceImpl.java
+++ src/main/java/com/tienda/service/impl/ProductoServiceImpl.java
@@ -48,4 +48,16 @@ public class ProductoServiceImpl implements ProductoService {
         return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
     }
 
+    @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup) {
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }
+    
+    @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoNativo(double precioInf, double precioSup) {
+        return productoDao.metodoNativo(precioInf, precioSup);
+    }
+    
 }
